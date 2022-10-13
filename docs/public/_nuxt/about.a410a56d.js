43f4757e2091c26c129e02e7bc0e1039a339970c
takuyaw-w <takuya.jamtb@gmail.com>
2022-10-13 23:06:36 +0900

    wip

---
+++ docs/public/_nuxt/about.a410a56d.js
@@ -0,0 +1,1 @@
+import{a as e,o,b as t}from"./entry.516e18be.js";const c={};function a(n,r){return o(),t("h1",null,"about")}const _=e(c,[["render",a]]);export{_ as default};
---
+++ docs/public/_nuxt/index.247e8e73.js
@@ -0,0 +1,1 @@
+import{a as e,o as n,b as c}from"./entry.516e18be.js";const o={};function r(t,a){return n(),c("h1",null,"index")}const _=e(o,[["render",r]]);export{_ as default};
